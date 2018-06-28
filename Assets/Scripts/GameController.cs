32a1357ba00553ccd3c2ed770925fd82302a72e3
jiang jie <2822569160@qq.com>
2018-06-28 21:18:26 +0800

    箭头相关

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,7 +7,14 @@ public class GameController : MonoBehaviour {
     public float angle;
     public Vector3 arrowPos;
 
-    
+
+
+
+    private void Update()
+    {
+        OnWork();
+    }
+
     private void OnWork()
     {
         if (Input.GetMouseButtonDown(0))
@@ -24,8 +31,10 @@ public class GameController : MonoBehaviour {
             {
                 if (hit.collider.tag == "1")
                 {
-                    Instantiate(arrow, arrowPos, Quaternion.identity);
-                    TestForRotation();
+                    TestForRotation();      
+                    Instantiate(arrow, arrowPos, Quaternion.Euler(0,0,angle));
+                     
+                    
                 }
             }
         }
@@ -34,14 +43,19 @@ public class GameController : MonoBehaviour {
     {
         GameObject pointA = GameObject.Find("Land2");
         GameObject pointB = GameObject.Find("Land1");
-        Vector3 vecA = pointA.transform.position;
+        Vector3 vecA = pointA.GetComponent<Transform>().position;
         Vector3 vecB = pointB.GetComponent<Transform>().position;//两种写法都体验一下
         arrowPos = (vecA + vecB) / 2;
         float distance = (vecB - vecA).magnitude;//两点间距离计算
         arrow.transform.localScale = new Vector3(distance / 6, 1, 1f);
         Vector3 direction = vecB - vecA;                                    //终点减去起点  
-        angle = Vector3.Angle(direction, Vector3.right);              //计算旋转角度  
-        arrow.GetComponent<Transform>().Rotate(0, 0, angle);
+        angle = Vector3.Angle(direction, Vector3.right);              //计算旋转角度
+        direction = Vector3.Normalize(direction);                           // 向量规范化
+        float dot = Vector3.Dot(direction, Vector3.up);                  // 判断是否Vector3.right在同一方向
+        if (dot < 0)
+            angle = 360 - angle;
+        //arrow.GetComponent<Transform>().Rotate(0, 0, angle);
+
 
 
     }
