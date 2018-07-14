d174822d792834238ca21117d5492f8446c896df
jiang jie <2822569160@qq.com>
2018-07-14 12:34:24 +0800

    add soldier

---
+++ Assets/Scripts/Cards/WuZhuang.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WuZhuang : Card {
+    public void CardEffect(int playerID, GameObject[] targets)
+    {
+        
+        //TODO:卡牌的操作
+        foreach(GameObject target in targets)
+        {
+            target.GetComponent<Map>().AddSoldier();
+        }
+    }
+}
